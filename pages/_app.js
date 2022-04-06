7e595c1c25666f4bf1b7e6491bf563a1b43979ca
nmimran99 <nmimran99@gmail.com>
2022-04-06 17:23:32 -0400

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Layout from "../components/Layout";
 import { ModalsContextProvider } from "../contexts/modalContext";
 import { SnackbarContextProvider } from "../contexts/snackbarContext";
@@ -8,6 +9,12 @@ function MyApp({ Component, pageProps }) {
 		<SnackbarContextProvider>
 			<ModalsContextProvider>
 				<Layout>
+					<Head>
+						<meta
+							name="viewport"
+							content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
+						></meta>
+					</Head>
 					<Component {...pageProps} />
 				</Layout>
 			</ModalsContextProvider>
