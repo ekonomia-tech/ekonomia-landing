5fef0ec45b171c5f36006d1df820e56bcd34bf0f
nmimran99 <nmimran99@gmail.com>
2022-05-25 10:21:03 -0400

    minor fixes

--- components/home/HexagonContainer.js
+++ components/home/HexagonContainer.js
@@ -1,8 +1,8 @@
 export default function HexagonContainer({ icon, header, content }) {
 	return (
-		<div className="relative flex justify-center">
+		<div className="relative flex justify-center my-4">
 			<img src="svg/HexData.svg" className="w-[400px] sm:w-[500px]" />
-			<div className="absolute top-28 left-1/2 -translate-x-1/2 flex flex-col items-center md:top-24 lg:top-32">
+			<div className="absolute top-20 left-1/2 -translate-x-1/2 flex flex-col items-center lg:top-28">
 				<img
 					src={icon}
 					className="mb-4 w-[32px] sm:w-[40px] sm:my-8 md:my-4 "
@@ -10,7 +10,7 @@ export default function HexagonContainer({ icon, header, content }) {
 				<div className=" font-thin text-center text-gray-100 text-2xl w-max ">
 					{header}
 				</div>
-				<div className=" font-thin text-center text-gray-100 my-4 text-xs w-[280px] sm:w-[300px] sm:text-sm md:my-4">
+				<div className=" font-thin text-center text-gray-100 my-4 text-xs w-[240px] sm:w-[300px] lg:text-sm md:my-4">
 					{content}
 				</div>
 			</div>
